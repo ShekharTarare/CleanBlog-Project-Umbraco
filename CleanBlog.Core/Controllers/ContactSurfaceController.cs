484247a5092de1221319615b3624d912e8b90fdb
ShekharTarare <ShekharTarare@DESKTOP-MLF9VLT>
2023-01-03 02:23:17 +0530

    Add project files.

---
+++ CleanBlog.Core/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,50 @@
+﻿using CleanBlog.Core.ViewModels;
+using Umbraco.Core.Logging;
+using System;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+using CleanBlog.Core.Services;
+
+namespace CleanBlog.Core.Controllers
+{
+    public class ContactSurfaceController : SurfaceController
+    {
+        private readonly ISmtpService _service;
+        //We need to register below service so now we have to use composers to register this service
+        public ContactSurfaceController(ISmtpService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult RenderForm()
+        {
+            ContactViewModel model = new ContactViewModel() { ContactFormId = CurrentPage.Id };
+            return PartialView("~/Views/Partials/Contact/contactForm.cshtml", model);
+        }
+
+        [HttpPost]
+        public ActionResult RenderForm(ContactViewModel model)
+        {
+            return PartialView("~/Views/Partials/Contact/contactForm.cshtml", model);
+        }
+
+        public ActionResult SubmitForm(ContactViewModel model)
+        {
+            bool success = false;
+            if (ModelState.IsValid)
+            {
+                success = _service.SendEmail(model);
+            }
+
+            var contactPage = UmbracoContext.Content.GetById(false, model.ContactFormId);
+
+            var successMessage = contactPage.Value<IHtmlString>("successMessage");
+            var errorMessage = contactPage.Value<IHtmlString>("errorMessage");
+
+            return PartialView("~/Views/Partials/Contact/result.cshtml", success ? successMessage : errorMessage);
+        }
+    }
+}
---
+++ CleanBlog.Core/Services/SmtpService.cs
@@ -0,0 +1,30 @@
+﻿using CleanBlog.Core.Controllers;
+using CleanBlog.Core.ViewModels;
+using System;
+using Umbraco.Core.Logging;
+
+namespace CleanBlog.Core.Services
+{
+    public class SmtpService : ISmtpService
+    {
+        private readonly ILogger _logger;
+        public SmtpService(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public bool SendEmail(ContactViewModel model)
+        {
+            try
+            {
+                return true;
+                //Umbraco 8 provides logs by default
+                //I am not writing the mail code here for now
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(typeof(ContactSurfaceController), ex, "Error sending contact form.");
+                return false;
+            }
+        }
+    }
+}
